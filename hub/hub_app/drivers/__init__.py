b13d4d33e97a01aab88104070ccaf0560a44f057
Vojta Liska <vliska@isibrno.cz>
2025-10-07 17:06:03 +0200

    very basic tgf4000 support

--- hub/hub_app/drivers/__init__.py
+++ hub/hub_app/drivers/__init__.py
@@ -31,3 +31,9 @@ try:
 except Exception as e:
     PicoScope5000a = None
     print(f" - could not import PicoScope5000a driver: {e}")
+
+try:
+    from .aimtt.tgf4000 import TGF4000
+except Exception as e:
+    TGF4000 = None
+    print(f" - could not import TGF4000 driver: {e}")
